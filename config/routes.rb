893f53f82517cc0dc886c328d65225c8592609fa
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-04-24 10:30:18 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # get '/students', to: 'students#index'
+
+  # get "/", to: 'application#welcome'
+
+  get '/students/:id/activate', to: 'students#activate', as: 'activate_student'
+
+  resources :students, only: [:index, :show]
+
 end
